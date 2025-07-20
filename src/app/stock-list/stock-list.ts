5e66693408d4f61573e50946514118a6080afd8a
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-07-20 12:39:18 -0500

    Enhance stock list component with improved item layout and button styles

--- src/app/stock-list/stock-list.ts
+++ src/app/stock-list/stock-list.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { StocksService } from '../stocks.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.html',
-  styleUrls: ['./stock-list.css'], // <-- This makes your CSS work!
+  styleUrls: ['./stock-list.css'],
   imports: [CommonModule, FormsModule]
 })
 export class StockListComponent implements OnInit {
   stocks: any[] = [];
   search: string = '';
+  addedStockSymbols: Set<string> = new Set();
 
-  constructor(private stocksService: StocksService) {}
+  constructor(
+    private stocksService: StocksService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.loadStocks();
@@ -21,11 +26,27 @@ export class StockListComponent implements OnInit {
 
   loadStocks() {
     this.stocksService.getStocks(this.search).subscribe(data => {
+      // Optionally: mark stocks as 'added' for UI
+      data.forEach((stock: any) => {
+        stock.added = this.addedStockSymbols.has(stock.symbol);
+      });
       this.stocks = data;
     });
   }
 
   onSearchChange() {
     this.loadStocks();
+  }
+
+  addToPortfolio(stock: any) {
+    this.http.post('/portfolio_stocks', { symbol: stock.symbol }).subscribe({
+      next: () => {
+        stock.added = true;
+        this.addedStockSymbols.add(stock.symbol);
+      },
+      error: () => {
+        alert('Could not add to portfolio.');
+      }
+    });
   }
 }
