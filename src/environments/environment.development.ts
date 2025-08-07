21707496eec2b64ad0de403a9199fb7dd0b52dd2
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-08-07 17:20:33 -0500

    fix: update API URL in development environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000'
+    //apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://stock-tracker-api-zy5k.onrender.com'
 };
