59b96b20e4428f2c6a00be136d9b20bf98dfa67b
Matthew Higgins <156959517+mthiggins1s@users.noreply.github.com>
2025-08-15 10:36:34 -0500

    feat: add token interceptor and update app configuration for HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
 // app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { tokenInterceptor } from './core/interceptors/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([
+        tokenInterceptor
+      ])
+    ),
     provideAnimations()
   ]
 };
